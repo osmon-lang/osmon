083f73a5407a04562d6561204436397a898d284f
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2023-10-11 15:48:39 +0500

    nix shell configuration

---
+++ shell.nix
@@ -0,0 +1,33 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  # Extract the lib folder from a package
+  getLibFolder = pkg: "${pkg}/lib";
+  libiconvPath = "${pkgs.libiconv}/lib";
+in
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.gcc
+    pkgs.rustc
+    pkgs.cargo
+    pkgs.libiconv
+    pkgs.libgccjit
+    pkgs.llvmPackages.llvm
+    pkgs.llvmPackages.clang
+  ];
+
+  # Set the LD_LIBRARY_PATH so that the dynamic linker can find shared libraries
+  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+    (getLibFolder pkgs.gcc)
+    (getLibFolder pkgs.llvmPackages.llvm)
+    libiconvPath
+  ];
+
+  NIX_LDFLAGS = "-L${libiconvPath} -L${./lib}";
+
+  shellHook = ''
+    export DYLD_LIBRARY_PATH="${./lib}:$DYLD_LIBRARY_PATH"
+
+    echo "Loaded development environment with libgccjit, Rust, GCC, LLVM, Clang!"
+  '';
+}
