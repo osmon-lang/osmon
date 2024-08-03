b0905f292aab034afbf05125f991df38a83abe74
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-08-03 19:38:18 +0500

    additional meta descriptions

--- default.nix
+++ default.nix
@@ -25,4 +25,26 @@ pkgs.rustPlatform.buildRustPackage rec {
   ];
 
   NIX_LDFLAGS = "-L${libiconvPath} -L${./lib}";
+
+  meta = with lib; {
+    homepage = "https://osmon-lang.uz";
+    description = ''
+      Highly experimental programming language developed by @orzklv.";
+    '';
+    licencse = licenses.mit;
+    platforms = with platforms; linux ++ darwin;
+
+    maintainers = [
+      {
+        name = "Sokhibjon Orzikulov";
+        email = "sakhib@orzklv.uz";
+        handle = "orzklv";
+        github = "orzklv";
+        githubId = 54666588;
+        keys = [{
+          fingerprint = "00D2 7BC6 8707 0683 FBB9  137C 3C35 D3AF 0DA1 D6A8";
+        }];
+      }
+    ];
+  };
 }
