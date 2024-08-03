89e74b2b8d9287aac0b11ce2cd91e54db17c9f59
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-08-03 19:30:08 +0500

    adding cargo lock for build

--- default.nix
+++ default.nix
@@ -1,10 +1,12 @@
 { pkgs ? import <nixpkgs> { } }:
 let
+  getLibFolder = pkg: "${pkg}/lib";
+  libiconvPath = "${pkgs.libiconv}/lib";
   manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
 in
 pkgs.rustPlatform.buildRustPackage rec {
   pname = manifest.name;
-  version = "manifest.version;
+  version = manifest.version;
   cargoLock.lockFile = ./Cargo.lock;
   src = pkgs.lib.cleanSource ./.;
 
@@ -15,4 +17,12 @@ pkgs.rustPlatform.buildRustPackage rec {
     pkgs.llvmPackages.llvm
     pkgs.llvmPackages.clang
   ];
+
+  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+    (getLibFolder pkgs.gcc)
+    (getLibFolder pkgs.llvmPackages.llvm)
+    libiconvPath
+  ];
+
+  NIX_LDFLAGS = "-L${libiconvPath} -L${./lib}";
 }
